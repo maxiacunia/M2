3f98804854f9c15ea951013bd55f3d71cc94aa2b
Maximiliano <maxi950live@gmail.com>
2022-10-09 00:13:37 -0300

    Tarea terminada

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,12 @@
+
+export default function About(){
+    return (
+        <div>
+            <h3>Info Clima</h3>
+            <h5>El clima se define como las condiciones meteorológicas medias que caracterizan 
+                a un lugar determinado.​​ Es una síntesis del tiempo atmosférico, obtenida a partir 
+                de estadísticas a largo plazo. Los elementos meteorológicos a tomar en cuenta son 
+                la temperatura, la presión, el viento, la humedad y la precipitación.</h5>
+        </div>
+    )
+}

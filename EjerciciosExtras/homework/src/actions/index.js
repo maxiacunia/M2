f7af0e6540ad2b9b33670e2b4390f8068af0154a
Maximiliano <maxi950live@gmail.com>
2022-10-14 11:55:23 -0300

    Actions listas

--- EjerciciosExtras/homework/src/actions/index.js
+++ EjerciciosExtras/homework/src/actions/index.js
@@ -1,5 +1,39 @@
-export const GET_ALL_POSTS = '';
-export const GET_ALL_USERS = '';
-export const GET_ALL_USERS_POST = '';
-export const GET_ALL_COMMENTS_POST = '';
+export const GET_ALL_POSTS = 'GET_ALL_POSTS';
+export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_ALL_USERS_POST = 'GET_ALL_USERS_POST';
+export const GET_ALL_COMMENTS_POST = 'GET_ALL_COMMENTS_POST';
+
+export function getAllUsers() {
+    return function(dispatch) {
+        return fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(json => dispatch({ type: GET_ALL_USERS, payload: json }))
+    };
+}
+
+export function getAllUserPosts(id) {
+    return function(dispatch) {
+        return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    .then((response) => response.json())
+    .then(json => dispatch({ type: GET_ALL_USERS_POST, payload: json }))
+    };
+}
+
+//https://jsonplaceholder.typicode.com/comments?postId=1
+
+export function getAllCommentsPost(postId) {
+    return function(dispatch) {
+        return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+    .then((response) => response.json())
+    .then(data => dispatch({ type: GET_ALL_COMMENTS_POST, payload: data }))
+    };
+}
+
+export function getAllPosts() {
+    return function(dispatch) {
+        return fetch(`https://jsonplaceholder.typicode.com/post`)
+    .then((response) => response.json())
+    .then(data => dispatch({ type: GET_ALL_POSTS, payload: data }))
+    };
+}
 
